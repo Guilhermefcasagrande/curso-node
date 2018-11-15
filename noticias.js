9495d308aeebc37497c5f69582762f8482ba4444
Guilhermefcasagrande <guilhermefranciscocasagrande@gmail.com>
2018-11-15 16:45:10 -0200

    Requisições e respostas com http

--- noticias.js
+++ noticias.js
@@ -1,5 +1,15 @@
 var http = require('http');
 
 http.createServer(function(req, res){
-    res.end('<html><body>Portal de notícias</body></html>');
+    var categoria = req.url;
+
+    if(categoria == '/tecnologia'){
+        res.end('<html><body>Notícias de tcnologia</body></html>');
+    } else if(categoria == '/moda'){
+        res.end('<html><body>Notícias de moda</body></html>');
+    } else if(categoria == '/beleza'){
+        res.end('<html><body>Notícias de beleza</body></html>');
+    } else {
+        res.end('<html><body>Portal de notícias</body></html>');
+    }
 }).listen(3000);
