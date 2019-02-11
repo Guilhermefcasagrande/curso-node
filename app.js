e74afc713bf5f036e835f6ee5e0a9b049886ac58
Guilherme Francisco Casagrande <guilhermefranciscocasagrande@gmail.com>
2019-02-11 20:26:29 -0200

    Funcionamento dos módulos

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+// É uma boa prática usar o ./ quando o arquivo estiver no mesmo nível
+var msg = require('./mod_test');
+
 app.set('view engine', 'ejs');
 
+// Define as rotas
 app.get('/noticias', function(req, res){
     res.render('noticias/noticias');
 });
@@ -15,6 +19,9 @@ app.get('/', function(req, res){
     res.render('home/index');
 });
 
+// Cria o servidor
 app.listen(3000, function(){
-    console.log('Teste');
+    console.log(msg());
 });
+
+
---
+++ mod_test.js
@@ -0,0 +1,6 @@
+
+// Função utiizada para exportar um módulo
+module.exports = function(){
+    var msg = 'Este modulo contém apenas uma string';
+    return msg;
+}
